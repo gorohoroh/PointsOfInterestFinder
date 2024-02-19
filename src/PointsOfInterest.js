0c858a73a28f4cfa3e61ef0da02db9e903943206
Jura Gorohovsky <gorohoroh@gmail.com>
2024-02-19 23:24:51 +0200

    Canvas and PointsOfInterest with only a title

---
+++ src/PointsOfInterest.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Typography} from 'antd';
+
+const {Title} = Typography;
+
+function PointsOfInterest() {
+
+    return (
+        <div className="content">
+            <div className="title">
+                <Title>Points of Interest</Title>
+            </div>
+        </div>
+    )
+}
+
+export default PointsOfInterest;
