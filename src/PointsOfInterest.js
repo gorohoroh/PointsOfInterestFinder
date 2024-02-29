8541c4f04ef272fcf9b7095f82401aff81836d74
Jura Gorohovsky <gorohoroh@gmail.com>
2024-02-29 16:21:00 +0600

    Calling the Points of Interest API and displaying results as regular map markers

--- src/PointsOfInterest.js
+++ src/PointsOfInterest.js
@@ -1,15 +1,26 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Typography} from 'antd';
-import {Map, ZoomControl} from "pigeon-maps";
+import {Map, Marker, ZoomControl} from "pigeon-maps";
 import './styles.scss';
 import predefinedLocations from "./data/regions"
+import {getPointsOfInterestBySquare} from "./data/amadeus";
 
 const {Title} = Typography;
 
 function PointsOfInterest() {
     const [city, setCity] = useState(predefinedLocations.find(x => x.name === "Barcelona"));
+    const [pointsOfInterest, setPointsOfInterest] = useState();
 
-    return (
+    const getPointsOfInterest = async () => {
+        const poi = await getPointsOfInterestBySquare(city.location.square);
+        return setPointsOfInterest(poi);
+    }
+
+    useEffect(() => {
+        getPointsOfInterest()
+    }, [city])
+
+    return ( pointsOfInterest &&
         <div className="content">
             <div className="title">
                 <Title>Points of Interest</Title>
@@ -21,6 +32,16 @@ function PointsOfInterest() {
                      defaultCenter={[city.location.latitude, city.location.longitude]}
                      defaultZoom={13}>
                     <ZoomControl/>
+
+                    {pointsOfInterest.length > 0 && pointsOfInterest.map(location => {
+                        return (<Marker
+                                width={50}
+                                anchor={[location.geoCode.latitude, location.geoCode.longitude]}
+                            />
+
+                        );
+                    })}
+
                 </Map>
             </div>
         </div>
